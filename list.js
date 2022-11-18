ead135315f604d28e31b37a244113cc3b9911d47
jenanF <108689302+jenanF@users.noreply.github.com>
2022-11-19 02:12:54 +0300

    d

---
+++ list.js
@@ -0,0 +1,57 @@
+
+
+let mlist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < mlist.length; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  mlist[i].appendChild(span);
+}
+
+
+let close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+
+function newElement() {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("taskinp").value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    console.error('الرجاء ادخال قيمة');
+    alert('الرجاء ادخال قيمة');
+  } else {
+    document.getElementById("listdo").appendChild(li);
+  }
+  document.getElementById("taskinp").value = "";
+
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      let div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
--- spq.js
+++ spq.js
@@ -1,126 +1,145 @@
 
-let bok = document.getElementById("book");
+// let bok = document.getElementById("book");
 
-let questions = [
-    {
-        id:1,
-        question: "ما هو لونك المفضل و لماذا و كيف و ماذا؟",
-        options :[
-            "a",
-            "b",
-            "c",
-            "d"
-        ]
-    },
-    {
-        id:2,
-        question: "asd",
-        options :[
-            "a",
-            "b",
-            "c",
-            "d"
-        ]
-    },
-    {
-        id:3,
-        question: "asd",
-        options :[
-            "a",
-            "b",
-            "c",
-            "d"
-        ]
-    },
-    {
-        id:4,
-        question: "asd",
-        options :[
-            "a",
-            "b",
-            "c",
-            "d"
-        ]
-    },
-    {
-        id:5,
-        question: "asd",
-        options :[
-            "a",
-            "b",
-            "c",
-            "d"
-        ]
-    },
-    {
-        id:6,
-        question: "asd",
-        options :[
-            "a",
-            "b",
-            "c",
-            "d"
-        ]
-    },
+// const questions = [
+//     {
+//         id:1,
+//         question: "اي نوع من الالعاب تفضل؟",
 
-];
+//         options :[
+//             "الجماعية",
+//             "القتالية",
+//             "التنافسية",
+//             "الفردية"
+//         ]
+//     },
+//     {
+//         id:2,
+//         question: "هل تفضل ل",
+//         option1:"a",
 
-let Qcount = 0;
-let points = 0;
 
-window.onload = function(){
-    show(Qcount);
-};
+//         options :[
+//             "a",
+//             "b",
+//             "c",
+//             "d"
+//         ]
+//     },
+//     {
+//         id:3,
+//         question: "asd",
 
-function show(count){
-    let question = document.getElementById("quizb");
-    let[ op1, op2, op3, op4] = questions[count].options;
+//         options :[
+//             "a",
+//             "b",
+//             "c",
+//             "d"
+//         ]
+//     },
+//     {
+//         id:4,
+//         question: "asd",
 
-    question.innerHTML = `<h2 class="hcol">س${count + 1}.${questions[count].question}</h2>
-    <ul class="options_group">
 
-        <li class="option">${op1}</li>
-        <li class="option"> ${op2}</li>
-        <li class="option">${op3}</li>
-        <li class="option">${op4}</li>
+//         options :[
+//             "a",
+//             "b",
+//             "c",
+//             "d"
+//         ]
+//     },
+//     {
+//         id:5,
+//         question: "asd",
 
-   </ul>`;
-   toggleActive();
-}
+//         options :[
+//             "a",
+//             "b",
+//             "c",
+//             "d"
+//         ]
+//     },
+//     {
+//         id:6,
+//         question: "asd",
 
-function toggleActive(){
+//         options :[
+//             "a",
+//             "b",
+//             "c",
+//             "d"
+//         ]
+//     },
 
-    let option = document.querySelectorAll("li.option");
+// ];
 
-    for( let i=0; i< option.length; i++){
-        option[i].onclick = function(){
-            for( let i=0; i< option.length; i++){
 
-                if(option[i].classList.contains("active")){
-                    option[i].classList.remove("active")
-                }
-            }
-            option[i].classList.add("active");
-        }
-    }
-}
 
-function next(){
 
-    if(Qcount == question.length -1){
-        location.href = "final.html";
-    }
-    console.log(Qcount);
-}
-let answer = document.querySelector("li.option.active").innerHTML;
 
-if(answer == questions[Qcount].answer){
-    points +=10;
-    sessionStorage.setItem("points", points);
-}
 
-console.log(points);
 
-Qcount++;
+// let Qcount = 0;
+// let points = 0;
 
-show(Qcount);
+// window.onload = function(){
+//     show(Qcount);
+// };
+
+// function show(count){
+//     let question = document.getElementById("quizb");
+//     let[ op1, op2, op3, op4] = questions[count].options;
+
+//     question.innerHTML = `<h2 class="hcol">س${count + 1}.${questions[count].question}</h2>
+//     <ul class="options_group">
+
+//         <li class="option">${op1}</li>
+//         <li class="option"> ${op2}</li>
+//         <li class="option">${op3}</li>
+//         <li class="option">${op4}</li>
+
+//    </ul>`;
+//    toggleActive();
+// }
+
+
+// function toggleActive(){
+
+//     let option = document.querySelectorAll("li.option");
+
+//     for( let i=0; i< option.length; i++){
+//         option[i].onclick = function(){
+//             for( let i=0; i< option.length; i++){
+
+//                 if(option[i].classList.contains("active")){
+//                     option[i].classList.remove("active")
+//                 }
+//             }
+//             option[i].classList.add("active");
+//         }
+//     }
+// }
+
+
+// function next(){
+
+//     if(Qcount == question.length -1){
+//         location.href = "ruselt.html";
+//     }
+//     console.log(Qcount);
+// }
+// let answer = document.querySelector("li.option.active").innerHTML;
+
+// if(answer == questions[Qcount].answer){
+//     points +=10;
+//     sessionStorage.setItem("points", points);
+// }
+
+// console.log(points);
+
+// Qcount++;
+
+// show(Qcount);
+
+
